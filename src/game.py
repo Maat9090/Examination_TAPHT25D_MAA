a43b3f4ea996603e6fa01671a7e8b83c595c74ea
Maat9090 <Maat9090@gmail.com>
2026-03-08 12:03:52 +0100

    commit 4

--- src/game.py
+++ src/game.py
@@ -27,9 +27,18 @@ command = "a"
 while not command.casefold() in ["q", "x"]:
     print_status(g)
 
-    command = input("Use WASD to move, Q/X to quit. ")
+    command = input("Use WASD to move, I for inventory, Q/X to quit ")
     command = command.casefold()[:1]
+#------------------------------------------------------------------------------------------
+    #Nytt kommando: "i", skriver ut innehållet i spelarens inventory.
+    if command == "i":
+      print("Here is your inventory")
+      if not inventory:
+          print("Your inventory is empty.")
+      for item in inventory:
+          print(item.name)
 
+#___________________________________________________________________________________________
 #B.Förflyttningar i alla 4 riktningar. (Med tangenterna WASD.)
     if command == "d" and player.can_move(1, 0, g):  # move right
         # TODO: skapa funktioner, så vi inte behöver upprepa så mycket kod för riktningarna "W,A,S"
@@ -45,24 +54,47 @@ while not command.casefold() in ["q", "x"]:
             #g.set(player.pos_x, player.pos_y, g.empty)
             g.clear(player.pos_x, player.pos_y)
 
-
+    # ___________________________________________________________________________________________
     if command == "a" and player.can_move(-1, 0, g):  # move left
 
-        maybe_item = g.get(player.pos_x + 1, player.pos_y)
+        maybe_item = g.get(player.pos_x - 1, player.pos_y)
         player.move(-1, 0)
 
+        if isinstance(maybe_item, pickups.Item):
+            # we found something
+            score += maybe_item.value
+            inventory.append(maybe_item)      #Läggr till itm i inventory
+            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
+            #g.set(player.pos_x, player.pos_y, g.empty)
+            g.clear(player.pos_x, player.pos_y)
 
 
+   #___________________________________________________________________________________________
     if command == "w" and player.can_move(0, -1, g):  # move up
 
-        maybe_item = g.get(player.pos_x + 1, player.pos_y)
+        maybe_item = g.get(player.pos_x , player.pos_y-1)
         player.move(0, -1)
 
+        if isinstance(maybe_item, pickups.Item):
+            # we found something
+            score += maybe_item.value
+            inventory.append(maybe_item)      #Läggr till itm i inventory
+            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
+            #g.set(player.pos_x, player.pos_y, g.empty)
+            g.clear(player.pos_x, player.pos_y)
 
+    #___________________________________________________________________________________________
     if command == "s" and player.can_move(0, 1, g):  # move down
 
         maybe_item = g.get(player.pos_x + 1, player.pos_y)
         player.move(0, 1)
+        if isinstance(maybe_item, pickups.Item):
+            # we found something
+            score += maybe_item.value
+            inventory.append(maybe_item)      #Läggr till itm i inventory
+            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
+            #g.set(player.pos_x, player.pos_y, g.empty)
+            g.clear(player.pos_x, player.pos_y)
 
 # Hit kommer vi när while-loopen slutar
 print("Thank you for playing!")
