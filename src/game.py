8eeb2e104bc822b951f86950979617b3473ca104
Maat9090 <Maat9090@gmail.com>
2026-03-09 23:59:35 +0100

    commit 8

--- src/game.py
+++ src/game.py
@@ -1,3 +1,5 @@
+from tokenize import maybe
+
 from .grid import Grid
 from .player import Player
 from . import pickups
@@ -14,15 +16,13 @@ g.set_player(player)
 g.make_walls()
 pickups.randomize(g)
 
-
-
 command = "a"
 # Loopa tills användaren trycker Q eller X.
 while not command.casefold() in ["q", "x"]:
     print_status(g,score)
 
     command = input("Use WASD to move, I for inventory, Q/X to quit ")
-    command = command.casefold()[:1]
+  #  command = command.casefold()[:1]
 #------------------------------------------------------------------------------------------
     #Nytt kommando: "i", skriver ut innehållet i spelarens inventory.
     if command == "i":
@@ -31,83 +31,42 @@ while not command.casefold() in ["q", "x"]:
           print("Your inventory is empty.")
       for item in inventory:
           print(item.name)
+      continue
 
 #___________________________________________________________________________________________
-#B.Förflyttningar i alla 4 riktningar. (Med tangenterna WASD.)
-    if command == "d" and player.can_move(1, 0, g):  # move right
-        # TODO: skapa funktioner, så vi inte behöver upprepa så mycket kod för riktningarna "W,A,S"
-        maybe_item = g.get(player.pos_x + 1, player.pos_y)
-        # *************************************************************************************
-        if maybe_item == g.trap:  #Minska 10p på fälla
-            score -= 10
-        # *************************************************************************************
 
-        player.move(1, 0)
-        #*************************************************************************************
-        score -= 1    #G.The floor is lava - för varje steg man går ska man tappa 1 poäng.
-        # *************************************************************************************
-        if isinstance(maybe_item, pickups.Item):
-            # we found something
-            score += maybe_item.value
-            inventory.append(maybe_item)      #Läggr till itm i inventory
-            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
-            #g.set(player.pos_x, player.pos_y, g.empty)
-            g.clear(player.pos_x, player.pos_y)
+    dx=0 # x-led (höger/vänster)
+    dy=0 #y-led (upp/ner)
 
-    # ___________________________________________________________________________________________
-    if command == "a" and player.can_move(-1, 0, g):  # move left
+    if command == "d":
+        dx=1
+    elif command == "a":
+        dx=-1
+    elif command == "w":
+        dy=-1
+    elif command == "s":
+        dy=1
 
-        maybe_item = g.get(player.pos_x - 1, player.pos_y)
+    if (dx !=0 or dy !=0) and player.can_move(dx,dy, g):   #Om spelaren gå i den riktningen
+       new_x = player.pos_x + dx    #Vad blir för nästa ruta
+       new_y = player.pos_y + dy
+       maybe_item =g.get(new_x, new_y) #Kollr va som finns i rutan
+    #*************************************************************************************
 
-        if maybe_item == g.trap:  #Minska 10p på fälla
-            score -= 10
-        player.move(-1, 0)
-        score -= 1
+       if maybe_item == g.trap :  # Minska 10p på fälla
+          score -= 10
+    #*************************************************************************************
 
-        if isinstance(maybe_item, pickups.Item):
-            # we found something
-            score += maybe_item.value
-            inventory.append(maybe_item)      #*****Läggr till item i inventory*******
-            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
-            #g.set(player.pos_x, player.pos_y, g.empty)
-            g.clear(player.pos_x, player.pos_y)
+       player.move(dx, dy)
+   #*************************************************************************************
+       score -= 1  # G.The floor is lava - för varje steg man går ska man tappa 1 poäng.
+   # *************************************************************************************
+       if isinstance(maybe_item, pickups.Item):
+          # we found something
+           score += maybe_item.value
+           inventory.append(maybe_item)  # Läggr till itm i inventory
+           print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
+           # g.set(player.pos_x, player.pos_y, g.empty)
+           g.clear(player.pos_x, player.pos_y)
 
 
-   #___________________________________________________________________________________________
-    if command == "w" and player.can_move(0, -1, g):  # move up
-
-        maybe_item = g.get(player.pos_x , player.pos_y-1)
-
-        if maybe_item == g.trap:  #Minska 10p på fälla
-            score -= 10
-        player.move(0, -1)
-        score -= 1
-        if isinstance(maybe_item, pickups.Item):
-            # we found something
-            score += maybe_item.value
-            inventory.append(maybe_item)      #Läggr till itm i inventory
-            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
-            #g.set(player.pos_x, player.pos_y, g.empty)
-            g.clear(player.pos_x, player.pos_y)
-
-    #___________________________________________________________________________________________
-    if command == "s" and player.can_move(0, 1, g):  # move down
-
-        maybe_item = g.get(player.pos_x , player.pos_y+1)
-        if maybe_item == g.trap:  #Minska 10p på fälla
-            score -= 10
-
-        player.move(0, 1)
-
-
-        score -= 1
-        if isinstance(maybe_item, pickups.Item):
-            # we found something
-            score += maybe_item.value
-            inventory.append(maybe_item)      #Läggr till item i inventory
-            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
-            #g.set(player.pos_x, player.pos_y, g.empty)
-            g.clear(player.pos_x, player.pos_y)
-
-# Hit kommer vi när while-loopen slutar
-print("Thank you for playing!")
